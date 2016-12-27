84f26ed2f0fb14b8438ff6f0ce07433992ad0af7
George Antonious <george@antonious.ca>
2016-12-27 10:39:22 -0700

    Bind listeners to view cells using runtime reflection

---
+++ library/src/main/java/ca/antonious/viewcelladapter/BindListener.java
@@ -0,0 +1,16 @@
+package ca.antonious.viewcelladapter;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by George on 2016-12-22.
+ */
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface BindListener {
+    Class value();
+}
--- library/src/main/java/ca/antonious/viewcelladapter/ViewCellAdapter.java
+++ library/src/main/java/ca/antonious/viewcelladapter/ViewCellAdapter.java
@@ -1,11 +1,13 @@
 package ca.antonious.viewcelladapter;
 
+import android.support.v4.util.Pair;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -80,7 +82,38 @@ public class ViewCellAdapter extends RecyclerView.Adapter<BaseViewHolder> {
         AbstractViewCell viewCell = ViewCellUtils.getViewCell(sections, position);
 
         viewCell.bindListeners(holder, listenerCollection);
+        bindListeners(holder, viewCell);
         viewCell.bindViewCell(holder);
+    }
+
+    private void bindListeners(BaseViewHolder holder, AbstractViewCell viewCell) {
+        Class<?> viewCellClass = viewCell.getClass();
+
+        try {
+            Method[] methods = viewCellClass.getDeclaredMethods();
+
+            for (Method method: methods) {
+                if (method.isAnnotationPresent(BindListener.class)) {
+                    BindListener bindListener = method.getAnnotation(BindListener.class);
+
+                    Class[] paramTypes = method.getParameterTypes();
+                    Class<?> viewHolderParam = paramTypes[0];
+                    Class<?> listenerParam = paramTypes[1];
+
+                    // check params here
+
+                    Object listenerInstance = listenerCollection.getListener(bindListener.value());
+
+                    if (listenerInstance != null) {
+                        method.invoke(viewCell, holder, listenerInstance);
+                    }
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
     }
 
     @Override
