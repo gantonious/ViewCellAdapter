a20fd3204caa296a854fff76a5896e91e23de444
George Antonious <george@antonious.ca>
2017-04-26 02:53:25 -0600

    Add divider view cell

---
+++ viewcelladapter/src/main/java/ca/antonious/viewcelladapter/viewcells/builtins/MaterialDividerViewCell.java
@@ -0,0 +1,15 @@
+package ca.antonious.viewcelladapter.viewcells.builtins;
+
+import android.support.annotation.LayoutRes;
+
+import ca.antonious.viewcelladapter.R;
+
+/**
+ * Created by George on 2017-04-26.
+ */
+
+public class MaterialDividerViewCell extends StaticViewCell {
+    public MaterialDividerViewCell() {
+        super(R.layout.material_divider);
+    }
+}
