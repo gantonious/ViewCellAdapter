53205f1fabd084dbbe0df095f0b01ae63e0dfca1
George Antonious <george.antonious@shopify.com>
2017-05-28 12:59:25 -0400

    Update section decorator to handle notifications

--- viewcelladapter/src/main/java/ca/antonious/viewcelladapter/decorators/SectionDecorator.java
+++ viewcelladapter/src/main/java/ca/antonious/viewcelladapter/decorators/SectionDecorator.java
@@ -1,19 +1,26 @@
 package ca.antonious.viewcelladapter.decorators;
 
+import ca.antonious.viewcelladapter.internal.SectionObserver;
 import ca.antonious.viewcelladapter.sections.AbstractSection;
 
 /**
  * Created by George on 2016-12-17.
  */
 
-public abstract class SectionDecorator extends AbstractSection {
+public abstract class SectionDecorator extends AbstractSection implements SectionObserver {
     private AbstractSection decoratedSection;
 
     public SectionDecorator(AbstractSection decoratedSection) {
         this.decoratedSection = decoratedSection;
+        this.decoratedSection.addObserver(this);
     }
 
     public AbstractSection getDecoratedSection() {
         return decoratedSection;
+    }
+
+    @Override
+    public void onDataChanged() {
+        notifyDataChanged();
     }
 }
