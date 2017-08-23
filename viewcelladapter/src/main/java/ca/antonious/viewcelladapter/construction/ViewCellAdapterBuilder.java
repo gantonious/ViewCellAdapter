8af563f263d87f55b156911cc38e9be61bafcd9a
George Antonious <george.antonious@shopify.com>
2017-08-23 17:30:49 -0400

    Add SingletonSection

--- viewcelladapter/src/main/java/ca/antonious/viewcelladapter/construction/ViewCellAdapterBuilder.java
+++ viewcelladapter/src/main/java/ca/antonious/viewcelladapter/construction/ViewCellAdapterBuilder.java
@@ -2,6 +2,8 @@ package ca.antonious.viewcelladapter.construction;
 
 import ca.antonious.viewcelladapter.ViewCellAdapter;
 import ca.antonious.viewcelladapter.sections.AbstractSection;
+import ca.antonious.viewcelladapter.sections.SingletonSection;
+import ca.antonious.viewcelladapter.viewcells.AbstractViewCell;
 
 /**
  * Created by George on 2017-04-23.
@@ -21,6 +23,11 @@ public class ViewCellAdapterBuilder {
 
     public ViewCellAdapterBuilder disableUpdateOnDataChanges() {
         viewCellAdapter.setShouldUpdateOnSectionChanges(false);
+        return this;
+    }
+
+    public ViewCellAdapterBuilder singletonSection(AbstractViewCell viewCell) {
+        viewCellAdapter.add(new SingletonSection(viewCell));
         return this;
     }
 
